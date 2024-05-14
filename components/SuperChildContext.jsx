1204028699fea22ef3b41bd97e2aa919585dbef1
Harsh <hs5686584@gmail.com>
2024-05-14 23:17:53 +0530

    passing data from superChild to parent component

--- components/SuperChildContext.jsx
+++ components/SuperChildContext.jsx
@@ -1,13 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import React, {useContext} from 'react';
 import {GlobalInfo} from '../screens/UseContextScreen';
 
 export default function SuperChildContext() {
   const contextData = useContext(GlobalInfo);
   console.log(contextData);
+
+  const dataInSuperChild = 'super child data';
   return (
     <View>
       <Text style={{color: contextData.contextData}}>SuperChildContext</Text>
+      <Button
+        title="Superchild Button"
+        onPress={() => contextData.parentFunc(dataInSuperChild)}
+      />
     </View>
   );
 }
--- screens/UseContextScreen.jsx
+++ screens/UseContextScreen.jsx
@@ -1,17 +1,25 @@
 import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import React, {createContext, useState} from 'react';
 import ChildContext from '../components/ChildContext';
+import {get} from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 export const GlobalInfo = createContext();
 
 export default function UseContextScreen() {
   const [data, setData] = useState('red');
+  const [superChildData, setSuperChildData] = useState('');
+
+  const getData = item => {
+    console.log(item);
+    setSuperChildData(item);
+  };
   return (
     <SafeAreaView>
-      <GlobalInfo.Provider value={{contextData: data}}>
+      <GlobalInfo.Provider value={{contextData: data, parentFunc: getData}}>
         <View>
           <Text>useContext</Text>
           <ChildContext />
+          <Text>{superChildData}</Text>
         </View>
       </GlobalInfo.Provider>
     </SafeAreaView>
